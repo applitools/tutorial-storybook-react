f9689353bae4767d95bad789d11f9a713980885e
isonic1 <justin.ison@gmail.com>
2025-04-29 16:09:37 -0400

    small readme update

--- applitools.config.js
+++ applitools.config.js
@@ -5,8 +5,8 @@ module.exports = {
   batchName: 'Storybook React Tutorial',
   testConcurrency: 10,
   showLogs: false,
-  browser: [
-    { width: 1024, height: 768, name: 'chrome' }, // See https://applitools.com/tutorials/sdks/storybook/browsers for additional supported browsers and devices.
+  browser: [ // See https://applitools.com/tutorials/sdks/storybook/browsers for additional supported browsers and devices.
+    { width: 1024, height: 768, name: 'chrome' },
     // { width: 1024, height: 768, name: 'firefox' },
     // { chromeEmulationInfo: { deviceName: 'Galaxy S20' } },
  ] 
