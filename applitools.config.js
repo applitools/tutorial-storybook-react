dfdec70b5437de25dac05836dcea8904d207be87
Justin Ison <justin.ison@gmail.com>
2025-09-15 13:30:16 -0400

    Increase test concurrency from 10 to 100

--- applitools.config.js
+++ applitools.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   serverUrl: 'https://eyes.applitools.com',
   batchName: 'Storybook React Tutorial',
-  testConcurrency: 10,
+  testConcurrency: 100,
   showLogs: false,
   browser: [ // See https://applitools.com/tutorials/sdks/storybook/browsers for additional supported browsers and devices.
     { width: 1024, height: 768, name: 'chrome' },
