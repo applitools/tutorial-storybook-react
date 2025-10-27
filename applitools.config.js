981e902effeaa6ac09239db6bd80adad958d58df
isonic1 <justin.ison@gmail.com>
2025-10-27 17:27:31 -0400

    updated config file

--- applitools.config.js
+++ applitools.config.js
@@ -1,10 +1,14 @@
-// See https://applitools.com/tutorials/sdks/storybook/config#properties for additional configuration properties.
+// See https://applitools.com/docs/eyes/sdks/storybook/api/configuration for additional configuration properties.
+/**
+ * @type {import('@applitools/eyes-storybook').ApplitoolsConfig}
+ **/
+
 module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   serverUrl: 'https://eyes.applitools.com',
   batchName: 'Storybook React Tutorial',
   showLogs: false,
-  browser: [ // See https://applitools.com/tutorials/sdks/storybook/browsers for additional supported browsers and devices.
+  browser: [ // See https://applitools.com/docs/eyes/sdks/storybook/api/browsers for additional supported browsers and devices.
     { width: 1024, height: 768, name: 'chrome' },
     // { width: 1024, height: 768, name: 'firefox' },
     // { chromeEmulationInfo: { deviceName: 'Galaxy S20' } },
